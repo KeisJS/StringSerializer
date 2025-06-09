6c81478ba2b20fe546559a6ee5435e8f39cf51de
vitali <work@mail.com>
2025-06-10 01:30:37 +0300

    T-9: add bootstrap, add some classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
 import './App.css'
-import { benchmarks } from './benchmarks/benchmarks.ts';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { benchmarks } from './benchmarks/benchmarks.ts'
 
 function App() {
   
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>Тип теста</th>
-            <th>Исходная строка</th>
-            <th>Сжатая строка</th>
-            <th>Коэффециет сжатия, <br />(Длина сжатой стр / Длина исходной стр) * 100</th>
-          </tr>
-        </thead>
-        <tbody>
-        { benchmarks.map(({
-            description,
-            source,
-            serialized,
-            serializeData
-          }, index) => (
-          <tr key={index}>
-            <td>{ description }</td>
-            <td>{ source }</td>
-            <td>{ serialized }</td>
-            <td>{ serializeData }</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <table className="table table-striped">
+            <thead>
+            <tr className="align-middle">
+              <th>Тип теста</th>
+              <th>Исходная строка</th>
+              <th>Сжатая строка</th>
+              <th>
+                Коэффециет сжатия, %<br />
+                <span className="fw-lighter fst-italic">(Длина сжатой стр / Длина исходной стр) * 100</span>
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            { benchmarks.map(({
+                                description,
+                                source,
+                                serialized,
+                                serializeData
+                              }, index) => (
+              <tr key={index}>
+                <td>{ description }</td>
+                <td className="text-break">{ source }</td>
+                <td className="text-break">{ serialized }</td>
+                <td>{ serializeData }</td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
